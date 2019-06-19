0eaad6f835323f05bd2bb7c137167898ebf73fc4
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-06-19 14:00:13 +0300

    Подключена библиотека ModelMapper для конвертации Entity в Dto и обратно

---
+++ backend/src/main/java/ru/geekbrains/musicportal/config/AdditionalConfiguration.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.musicportal.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdditionalConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
