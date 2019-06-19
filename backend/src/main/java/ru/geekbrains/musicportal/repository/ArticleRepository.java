55321766c9193e5364b41c072107e936a3f6390b
nvfredy <nvfredy@gmail.com>
2019-06-19 20:26:25 +0500

    mp-028: Реализовал сущности статьи и комментария, к ним репозитории, сервисы и ДТО

--- backend/src/main/java/ru/geekbrains/musicportal/repository/ArticleRepository.java
+++ backend/src/main/java/ru/geekbrains/musicportal/repository/ArticleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import ru.geekbrains.musicportal.entity.Article;
 
 
+
 @Repository
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     Article findByTitle(String  title);
--- backend/src/main/java/ru/geekbrains/musicportal/repository/CommentRepository.java
+++ backend/src/main/java/ru/geekbrains/musicportal/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import ru.geekbrains.musicportal.entity.Comment;
 
 
+
 @Repository
 public interface CommentRepository extends CrudRepository<Comment,Long> {
 }
