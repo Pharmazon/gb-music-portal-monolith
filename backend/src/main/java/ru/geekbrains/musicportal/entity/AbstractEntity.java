06738e4f348d07c5f0bd646b00cec7923b2fedf7
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-06-21 16:07:58 +0300

    mp-022: продумана БД. Созданы все таблицы и связи. Созданы основные сущности в соответствии с БД

--- backend/src/main/java/ru/geekbrains/musicportal/entity/AbstractEntity.java
+++ backend/src/main/java/ru/geekbrains/musicportal/entity/AbstractEntity.java
@@ -1,25 +1,32 @@
 package ru.geekbrains.musicportal.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.util.Date;
 import java.util.UUID;
 
 @Getter
 @Setter
 @MappedSuperclass
+@NoArgsConstructor
 public abstract class AbstractEntity {
 
     @Id
-    @Column(name = "id")
-    private String id = UUID.randomUUID().toString();
+    @SequenceGenerator(name = "seq", sequenceName = "MUSIC_PORTAL_SEQUENCE", allocationSize = 1)
+    @GeneratedValue(generator = "seq_name", strategy = GenerationType.SEQUENCE)
+    protected Long id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "LAST_UPDATE", nullable = false)
+    private Date lastUpdate;
+
+    @Column(name = "CREATION_DATE", nullable = false)
+    private Date creationDate;
+
 
 }
