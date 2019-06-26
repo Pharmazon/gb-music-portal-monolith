23ed30824bd868a170163d90b3edeb2050d63a5b
annakhuseinova <annakhuseinova@yandex.ru>
2019-06-27 00:41:25 +0300

    feature-mp-058-creation-of-preloader

---
+++ frontend/miraculous-ui/src/assets/js/preloader.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+   let preloaderPicture = document.getElementById("preloader");
+   let appRoot = document.getElementsByTagName("app-root")[0];
+   preloaderPicture.style="display: none";
+   appRoot.style = "opacity: 1";
+
+}
