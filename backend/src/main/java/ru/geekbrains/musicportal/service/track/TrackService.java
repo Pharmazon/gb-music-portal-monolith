f21b8e51d43c297e2766764cbe9f0b08d4599461
Sergey Minenkov <nzpv11@gmail.com>
2019-06-28 13:53:08 +0300

    mp-048: Добавлен механизм динамической фильтрации треков по заданным наборам условий. Механизм можно расширить как внутри треков, так и на другие сущности базы.

--- backend/src/main/java/ru/geekbrains/musicportal/service/track/TrackService.java
+++ backend/src/main/java/ru/geekbrains/musicportal/service/track/TrackService.java
@@ -1,7 +1,12 @@
 package ru.geekbrains.musicportal.service.track;
 
+import org.springframework.data.domain.Page;
+import ru.geekbrains.musicportal.entity.common.SpecFeature;
 import ru.geekbrains.musicportal.entity.track.Track;
 import ru.geekbrains.musicportal.service.common.CommonService;
 
+import java.util.List;
+
 public interface TrackService extends CommonService<Track> {
+    Page<Track> getTrackWithPagingAndFiltering(int pageNumber, int pageSize, List<SpecFeature> specFeatures);
 }
