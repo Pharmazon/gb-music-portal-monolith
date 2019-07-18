e0f99772de40973a9dc2c78f6fbc11453ac450e7
annakhuseinova <annakhuseinova@yandex.ru>
2019-07-18 04:14:51 +0300

    mp-110. Создан интерфейс для админки

---
+++ frontend/miraculous-ui/src/app/admin-page/featured-music-component/featured-music-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'featured-music-component',
+  templateUrl: './featured-music-component.component.html',
+  styleUrls: ['./featured-music-component.component.css']
+})
+export class FeaturedMusicComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/miraculous-ui/src/app/admin-page/users-component/users-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'users-component',
+  templateUrl: './users-component.component.html',
+  styleUrls: ['./users-component.component.css']
+})
+export class UsersComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
