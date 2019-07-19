3aefaec2924d72ef0bac625c8bb644c24daf8fa0
Andrey <nvfredy@gmail.com>
2019-07-19 18:59:00 +0500

    mp-105: Обновление LikeRestController, согласно документации

--- backend/src/main/java/ru/geekbrains/musicportal/controller/rest/LikeRestController.java
+++ backend/src/main/java/ru/geekbrains/musicportal/controller/rest/LikeRestController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.musicportal.dto.blog.LikeDto;
 import ru.geekbrains.musicportal.entity.blog.Like;
+import ru.geekbrains.musicportal.entity.track.Track;
+import ru.geekbrains.musicportal.enums.EntityLikeEnum;
 import ru.geekbrains.musicportal.marker.LikeViews;
 import ru.geekbrains.musicportal.service.blog.LikeServiceImpl;
+import ru.geekbrains.musicportal.service.track.TrackService;
+import ru.geekbrains.musicportal.service.track.TrackServiceImpl;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -18,10 +22,12 @@ import java.util.Optional;
 public class LikeRestController {
 
     private LikeServiceImpl likeService;
+    private TrackServiceImpl trackService;
 
     @Autowired
-    public LikeRestController(LikeServiceImpl likeService) {
+    public LikeRestController(LikeServiceImpl likeService, TrackServiceImpl trackService) {
         this.likeService = likeService;
+        this.trackService = trackService;
     }
 
     @JsonView(LikeViews.List.class)
@@ -44,8 +50,24 @@ public class LikeRestController {
     }
 
     @JsonView(LikeViews.List.class)
-    @PostMapping
-    public Like createLike(@Valid LikeDto like) {
-        return likeService.save(like);
+    @PostMapping ("{id}")
+    public Like createLike(@PathVariable Long id, @Valid LikeDto likeDto) {
+        return likeService.save(likeDto);
+    }
+
+    @JsonView(LikeViews.List.class)
+    @PostMapping ("tracks/add-like/{id}")
+    public Like createLikeTrack(@PathVariable Long id, @Valid LikeDto likeDto) {
+        likeDto.setEntityId(id);
+        likeDto.setEntity(EntityLikeEnum.TRACK);
+        return likeService.save(likeDto);
+    }
+
+    @JsonView(LikeViews.List.class)
+    @PostMapping ("albums/add-like/{id}")
+    public Like createLikeAlbum(@PathVariable Long id, @Valid LikeDto likeDto) {
+        likeDto.setEntityId(id);
+        likeDto.setEntity(EntityLikeEnum.ALBUM);
+        return likeService.save(likeDto);
     }
 }
